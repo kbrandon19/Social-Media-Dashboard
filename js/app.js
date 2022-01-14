3d6b68c6c9e80790b6dcba38febb7f87fc5ea2c2
Kenneth Brandon <kbrandon319@gmail.com>
2022-01-13 22:07:00 -0500

    Completion of toggle and hover actions

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 let checkbox = document.getElementById('checkbox');
 
 checkbox.addEventListener('change', function(event){
-    (event.target.checked) ? document.body.setAttribute('data-theme', 'dark') : document.body.removeAttribute('data-theme');
+    (event.target.checked) ? document.body.setAttribute('data-theme', 'dark') : 
+    document.body.removeAttribute('data-theme');
   });
 
